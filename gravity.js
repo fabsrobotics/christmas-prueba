c0fd47a271f129922b720250c10988a2e5cdf2c4
andresbr92 <andresbr92@gmail.com>
2020-12-14 11:20:14 +0100

    add

--- gravity.js
+++ gravity.js
@@ -65,7 +65,7 @@ var Example = Example || {};
         stack,
         Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
         Bodies.rectangle(400, 600, 800, 50.5, { isStatic: true }),
-        Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
+        Bodies.rectangle(400, 300, 50, 600, { isStatic: true }),
         Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
         
     ]);
