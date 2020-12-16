097cac25a3b80e502300f591c313f249b4f8e8ab
andresbr92 <andresbr92@gmail.com>
2020-12-16 11:53:48 +0100

    alpha

--- gravity.js
+++ gravity.js
@@ -1,198 +1,132 @@
+function gravity() {
+  let Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Composites = Matter.Composites,
+    Common = Matter.Common,
+    MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Svg = Matter.Svg;
 
+  // create engine
+  let engine = Engine.create(),
+    world = engine.world;
 
-function gravity(){
-    let Engine = Matter.Engine,
-        Render = Matter.Render,
-        Runner = Matter.Runner,
-        Composites = Matter.Composites,
-        Common = Matter.Common,
-        MouseConstraint = Matter.MouseConstraint,
-        Mouse = Matter.Mouse,
-        World = Matter.World,
-        Bodies = Matter.Bodies,
-        Svg = Matter.Svg
+  //lenght and with
+  const w = window.visualViewport.width;
+  const h = window.visualViewport.height;
 
-    // create engine
-    let engine = Engine.create(),
-        world = engine.world;
-      
-    
-    //lenght and with
-    const w = window.visualViewport.width;
-    const h = window.visualViewport.height;
+  // create renderer
+  const canvasSnow = document.getElementById("canvasSnow");
+  let render = Render.create({
+    element: canvasSnow,
+    engine: engine,
+    options: {
+      width: w,
+      height: h,
+      wireframes: false,
+      background: "#ffffff00",
+    },
+  });
 
-    // create renderer
-    const canvasSnow = document.getElementById("canvasSnow")
-    let render = Render.create({
-        element: canvasSnow,
-        engine: engine,
-        options: {
-            width: w,
-            height: h,
-            wireframes: false,
-            background: "#ffffff00"
-        }
-    });
+  Render.run(render);
 
-    Render.run(render);
-    
+  // create runner
+  let runner = Runner.create();
+  Runner.run(runner, engine);
 
-    // create runner
-    let runner = Runner.create();
-    Runner.run(runner, engine);
+  let gravity = engine.world.gravity;
 
-    //const snowFlakesArray = []
-    let gravity = engine.world.gravity;
+  // snowBall
+  const sbRadius = w / 3;
+  const mfRadius = sbRadius / 30;
 
-    // snowBall
-   // const sbRadius = w/3;
-   // const SnowBall = Bodies.circle(w/2,h/2,sbRadius,{isStatic: true})
-    //World.add(world,[SnowBall])
+  let ballRadius = w / 2.9;
 
-    // miniflakes
-    // let mfArray = []
-    // const mfRadius = sbRadius/20;
-    // for(let i = 0; i < 100; i++){
-    //     let mfBall = Bodies.circle(w/2+i,h/2.9,mfRadius, {render: {fillStyle: "#0000ff",strokeStyle: "#fff",}})
+  for (let i = 0; i < 90; i++) {
+    a = Bodies.rectangle(
+      w / 2 + ballRadius * Math.cos((i * 4 * Math.PI) / 180),
+      h / 2 - 35 + ballRadius * Math.sin((i * 4 * Math.PI) / 180),
+      15,
+      15,
+      {
+        isStatic: true,
+        angle: (Math.PI / 180) * i * 4,
+        render: {
+          fillStyle: "#000",
+          strokeStyle: "#fff",
+          lineWidth: 0,
+          visible: 0,
+        },
+      }
+    );
+    World.add(engine.world, a);
+  }
 
-
-    //     mfArray.push(mfBall);
-
-    // }
-    // World.add(world,mfArray); 
-
-        let ballRadius =  w / 2.9
-
-        for(let i = 0; i < 90; i++) {
-            a = Bodies.rectangle(
-                w / 2  + ballRadius * Math.cos(i * 4 * Math.PI / 180), 
-                h / 2 - 35 + ballRadius * Math.sin(i * 4 * Math.PI / 180), 
-                15, 
-                15, 
-                {
-                    isStatic: true, 
-                    angle: Math.PI / 180 * i * 4,
-                    render: {
-                        fillStyle: "#000",
-                        strokeStyle: "#fff",
-                        lineWidth: 0,
-                         visible: 0,
-                    }
-                }
-            );
-            World.add(engine.world, a);
-        }
-       
-
-   
-
-   
-    setInterval(() => {
-        
-
-        gravity.x = Common.random(-0.1, 0.1)
-        gravity.y = 0.3
-        
-        World.add(world, [
-            Bodies.circle(Common.random(20, window.visualViewport.width), -10, Common.random(3,7), {render: {fillStyle: "#fff", strokeStyle: "#fff",}}),
-            
-            Bodies.circle(Common.random(20, window.visualViewport.width), -10, Common.random(3,7), {render: {fillStyle: "#fff", strokeStyle: "#fff",}}),
-            
-        ]);
-        
-        const allBodies = Matter.Composite.allBodies(world)
-        allBodies.forEach(element => {
-            
-
-            if(element.position.x >( window.visualViewport.width + 50) | element.position.x < 0) {
-                Matter.Composite.remove(world, element)
-            }
-            
-            
-            
-        });
-
-        
-    }, 400);
-
-
-
-   
-   
-
-
+  setInterval(() => {
+    gravity.x = Common.random(-0.1, 0.1);
+    gravity.y = 0.3;
 
     World.add(world, [
-        //snowFlakesArray,
-        //Bodies.rectangle(0, 0, window.visualViewport.width*2, 20, { isStatic: true }),
-        //Bodies.rectangle(0, 0, 20, window.visualViewport.height*2, { isStatic: true }),
-        //Bodies.rectangle(window.visualViewport.width, window.visualViewport.height/2, 20, window.visualViewport.height, { isStatic: true }),
-        Bodies.rectangle(0, window.visualViewport.height, window.visualViewport.width*2, 20, { isStatic: true }),
-        //stack,
+      Bodies.circle(
+        Common.random(20, window.visualViewport.width),
+        -10,
+        Common.random(mfRadius, mfRadius / 1.5),
+        { render: { fillStyle: "#fff", strokeStyle: "#fff" } }
+      ),
+
+      Bodies.circle(
+        Common.random(20, window.visualViewport.width),
+        -10,
+        Common.random(mfRadius, mfRadius / 1.5),
+        { render: { fillStyle: "#fff", strokeStyle: "#fff" } }
+      ),
     ]);
-   
-    
 
-    // add gyro control
-    // if (typeof window !== 'undefined') {
-    //     let updateGravity = function(event) {
-    //         let orientation = typeof window.orientation !== 'undefined' ? window.orientation : 0,
-    //             gravity = engine.world.gravity;
+    const allBodies = Matter.Composite.allBodies(world);
+    allBodies.forEach((element) => {
+      if (
+        (element.position.x > window.visualViewport.width + 50) |
+        (element.position.x < 0)
+      ) {
+        Matter.Composite.remove(world, element);
+      }
+    });
+  }, 400);
 
-    //         if (orientation === 0) {
-    //             gravity.x = Common.clamp(event.gamma, -90, 90) / 90;
-    //             gravity.y = Common.clamp(event.beta, -90, 90) / 90;
-    //         } else if (orientation === 180) {
-    //             gravity.x = Common.clamp(event.gamma, -90, 90) / 90;
-    //             gravity.y = Common.clamp(-event.beta, -90, 90) / 90;
-    //         } else if (orientation === 90) {
-    //             gravity.x = Common.clamp(event.beta, -90, 90) / 90;
-    //             gravity.y = Common.clamp(-event.gamma, -90, 90) / 90;
-    //         } else if (orientation === -90) {
-    //             gravity.x = Common.clamp(-event.beta, -90, 90) / 90;
-    //             gravity.y = Common.clamp(event.gamma, -90, 90) / 90;
-    //         }
-    //     };
+  World.add(world, [
+    Bodies.rectangle(
+      0,
+      window.visualViewport.height,
+      window.visualViewport.width * 2,
+      20,
+      { isStatic: true }
+    ),
+    ,
+  ]);
 
-    //     window.addEventListener('deviceorientation', updateGravity);
-    // }
-
-    // add mouse control
-    let mouse = Mouse.create(render.canvas),
-        mouseConstraint = MouseConstraint.create(engine, {
-            mouse: mouse,
-            constraint: {
-                stiffness: 0.2,
-                render: {
-                    visible: false
-                }
-            }
-        });
-
-    World.add(world, mouseConstraint);
-
-    // keep the mouse in sync with rendering
-    render.mouse = mouse;
-
-    // fit the render viewport to the scene
-    Render.lookAt(render, {
-        min: { x: 0, y: 0 },
-        max: { x: window.visualViewport.width, y: window.visualViewport.height }
+  // add mouse control
+  let mouse = Mouse.create(render.canvas),
+    mouseConstraint = MouseConstraint.create(engine, {
+      mouse: mouse,
+      constraint: {
+        stiffness: 0.2,
+        render: {
+          visible: false,
+        },
+      },
     });
 
-    // context for MatterTools.Demo
-    // return {
-    //     engine: engine,
-    //     runner: runner,
-    //     render: render,
-    //     canvas: render.canvas,
-    //     stop: function() {
-    //         Matter.Render.stop(render);
-    //         Matter.Runner.stop(runner);
-    //         if (typeof window !== 'undefined') {
-    //             window.removeEventListener('deviceorientation', updateGravity);
-    //         }
-    //     }
-    // };
+  World.add(world, mouseConstraint);
 
+  // keep the mouse in sync with rendering
+  render.mouse = mouse;
+
+  // fit the render viewport to the scene
+  Render.lookAt(render, {
+    min: { x: 0, y: 0 },
+    max: { x: window.visualViewport.width, y: window.visualViewport.height },
+  });
 }
--- snowball.js
+++ snowball.js
@@ -1,133 +1,137 @@
+function snowBall() {
+  let Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    World = Matter.World,
+    Common = Matter.Common,
+    Bodies = Matter.Bodies,
+    Body = Matter.Body;
 
-    function snowBall(){
-    let Engine = Matter.Engine,
-        Render = Matter.Render,
-        Runner = Matter.Runner,
-        World = Matter.World,
-        Common = Matter.Common,
-        Bodies = Matter.Bodies,
-        Body = Matter.Body
+  // create engine
+  let engine = Engine.create(),
+    world = engine.world;
 
-    // create engine
-    let engine = Engine.create(),
-        world = engine.world;
-    
-    
-    //lenght and with
-    const wBall = window.visualViewport.width;
-    const hBall = window.visualViewport.height;
+  //lenght and with
+  const wBall = window.visualViewport.width;
+  const hBall = window.visualViewport.height;
 
-    // create renderer
-    const canvasBall = document.getElementById("canvasBall")
-    let render = Render.create({
-        element: canvasBall,
-        engine: engine,
-        options: {
-            width: wBall,
-            height: hBall,
-            wireframes: false,
-            background: "#ffffff00"
-        }
-    });
+  // create renderer
+  const canvasBall = document.getElementById("canvasBall");
+  let render = Render.create({
+    element: canvasBall,
+    engine: engine,
+    options: {
+      width: wBall,
+      height: hBall,
+      wireframes: false,
+      background: "#ffffff00",
+    },
+  });
 
-    Render.run(render);
-    
+  Render.run(render);
 
-    // create runner
-    let runner = Runner.create();
-    Runner.run(runner, engine);
+  // create runner
+  let runner = Runner.create();
+  Runner.run(runner, engine);
 
-    const snowFlakesArray = []
-    let gravity = world.gravity;
-    //gravity.y = -0.1
-    
+  const snowFlakesArray = [];
+  let gravity = world.gravity;
 
-    // snowBall
-    const sbRadius = wBall/3;
-   // const SnowBall = Bodies.circle(w/2,h/2,sbRadius,{isStatic: true})
-    //World.add(world,[SnowBall])
+  // snowBall
+  const sbRadius = wBall / 3;
 
-    // miniflakes
-    let mfArray = []
-    const mfRadius = sbRadius/30;
-    for(let i = 0; i < 200; i++){
-        let mfBall = Bodies.circle(wBall/2, hBall / 2 + 90 * Math.sin(i * 4 * Math.PI / 180), mfRadius, {render: {fillStyle: "#fff", strokeStyle: "#fff",}})
+  // miniflakes
+  let mfArray = [];
+  const mfRadius = sbRadius / 30;
+  for (let i = 0; i < 200; i++) {
+    let mfBall = Bodies.circle(
+      wBall / 2,
+      hBall / 2 + 90 * Math.sin((i * 4 * Math.PI) / 180),
+      Common.random(mfRadius, mfRadius / 1.3),
+      { render: { fillStyle: "#fff", strokeStyle: "#fff" } }
+    );
 
-
-        mfArray.push(mfBall);
-
-    }
-    World.add(world,mfArray); 
-    let innerBallRadius = wBall/8.5
-    World.add(world, Bodies.circle(wBall/2, hBall/2 -35, innerBallRadius, 
-        {
-            isStatic:true,
-            render:{
-                visible: 0
-            }
-    }))
-
-        let ballRadius =  wBall / 2.9
-
-        for(let i = 0; i < 90; i++) {
-            a = Bodies.rectangle(
-                wBall / 2  + ballRadius * Math.cos(i * 4 * Math.PI / 180), 
-                hBall / 2 -35 + ballRadius * Math.sin(i * 4 * Math.PI / 180), 
-                20, 
-                20, 
-                {
-                    isStatic: true, 
-                    angle: Math.PI / 180 * i * 4,
-                    render: {
-                        fillStyle: "#000",
-                        strokeStyle: "#fff",
-                        lineWidth: 0,
-                         visible: 1,
-                    }
-                }
-            );
-            World.add(world, a);
-        }
-        gravity.y = 0
-
-setInterval(() => {
-    console.log(valGravityX, valGravityY)
-    valGravityX != undefined ? gravity.x = ((valGravityX * (-1)) / 100) : gravity.x = 0
-    valGravityY != undefined ? gravity.y = (valGravityY / 100) : gravity.y = 0.05
-
-    
-
-    mfArray.forEach(ball =>{
-        
-        
-        if (valForceX > 3) {
-            Body.applyForce (ball,  {x: ball.position.x, y: ball.position.y},{x:0.002, y: 0})
-            return
-            
-        } else if (valForceX < -3) {
-
-            Body.applyForce (ball,  {x: ball.position.x, y: ball.position.y},{x:-0.002, y: 0})
-            return
-
-        } else if (valForceY > 3) {
-            Body.applyForce (ball,  {x: ball.position.x, y: ball.position.y},{x:0 , y: 0.002})
-            return
-
-        } else if (valForceY < -3) {
-            Body.applyForce (ball,  {x: ball.position.x, y: ball.position.y},{x:0 , y: -0.002})
-            return
-
-        }
-        
+    mfArray.push(mfBall);
+  }
+  World.add(world, mfArray);
+  let innerBallRadius = wBall / 8.5;
+  World.add(
+    world,
+    Bodies.circle(wBall / 2, hBall / 2 - 35, innerBallRadius, {
+      isStatic: true,
+      render: {
+        visible: 0,
+      },
     })
-}, 200);
+  );
 
+  let ballRadius = wBall / 2.9;
 
+  for (let i = 0; i < 90; i++) {
+    a = Bodies.rectangle(
+      wBall / 2 + ballRadius * Math.cos((i * 4 * Math.PI) / 180),
+      hBall / 2 - 35 + ballRadius * Math.sin((i * 4 * Math.PI) / 180),
+      20,
+      20,
+      {
+        isStatic: true,
+        angle: (Math.PI / 180) * i * 4,
+        render: {
+          fillStyle: "#000",
+          strokeStyle: "#fff",
+          lineWidth: 0,
+          visible: 0,
+        },
+      }
+    );
+    World.add(world, a);
+  }
 
-    // fit the render viewport to the scene
-    Render.lookAt(render, {
-        min: { x: 0, y: 0 },
-        max: { x: window.visualViewport.width, y: window.visualViewport.height }
+  setInterval(() => {
+    console.log(valGravityX, valGravityY);
+    valGravityX != undefined
+      ? (gravity.x = (valGravityX * -1) / 100)
+      : (gravity.x = 0);
+    valGravityY != undefined
+      ? (gravity.y = valGravityY / 100)
+      : (gravity.y = 0.05);
+
+    mfArray.forEach((ball) => {
+      if (valForceX > 3) {
+        Body.applyForce(
+          ball,
+          { x: ball.position.x, y: ball.position.y },
+          { x: 0.002, y: 0 }
+        );
+        return;
+      } else if (valForceX < -3) {
+        Body.applyForce(
+          ball,
+          { x: ball.position.x, y: ball.position.y },
+          { x: -0.002, y: 0 }
+        );
+        return;
+      } else if (valForceY > 3) {
+        Body.applyForce(
+          ball,
+          { x: ball.position.x, y: ball.position.y },
+          { x: 0, y: 0.002 }
+        );
+        return;
+      } else if (valForceY < -3) {
+        Body.applyForce(
+          ball,
+          { x: ball.position.x, y: ball.position.y },
+          { x: 0, y: -0.002 }
+        );
+        return;
+      }
     });
-    }
-  
+  }, 200);
+
+  // fit the render viewport to the scene
+  Render.lookAt(render, {
+    min: { x: 0, y: 0 },
+    max: { x: window.visualViewport.width, y: window.visualViewport.height },
+  });
+}
